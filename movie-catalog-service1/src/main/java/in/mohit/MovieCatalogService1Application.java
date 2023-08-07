8aa534851d88d3785a74f0f70f4e2173f4f9404b
mohit <bhadanimohit@1997@gmail.com>
2023-08-07 17:59:00 +0530

    ADDED 3 services

---
+++ movie-catalog-service1/src/main/java/in/mohit/MovieCatalogService1Application.java
@@ -0,0 +1,13 @@
+package in.mohit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatalogService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogService1Application.class, args);
+	}
+
+}
---
+++ movie-catalog-service1/src/test/java/in/mohit/MovieCatalogService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.mohit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
